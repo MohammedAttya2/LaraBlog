ab2cfab7d27ad9b716e260be0b2aacf1a4a58db3
MohammedAttya2 <mohammed.attya26@gmail.com>
2017-04-05 10:56:47 +0200

    add link to post creator

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,8 +32,8 @@
 
     <div class="blog-header">
       <div class="container">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        <h1 class="blog-title">My Blog</h1>
+        <p class="lead blog-description">Learn Laravel from scratch</p>
       </div>
     </div>
     <div class="container">
